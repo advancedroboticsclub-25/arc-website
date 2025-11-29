c329cc02d092736a62d69d821c2f919a7f0a213c
Sonu Alam <khanthedominator007@gmail.com>
2025-11-29 14:14:21 +0545

    final

--- src/app/Technorion5.0/page.js
+++ src/app/Technorion5.0/page.js
@@ -27,7 +27,7 @@ export default function Home() {
         <EventDetails/> 
         <RegistrationDataSlabs/>
       
-      
+      <RegistrationForms/>
         <EventFaqs />
         <AboutUs />
         <Footer/>
